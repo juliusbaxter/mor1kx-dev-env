86ee79c707322ed5065a992e209638c36fe17d00
Julius Baxter <juliusbaxter@gmail.com>
2013-04-17 00:18:54 +0100

    de0 nano: backend TCL pinout updates
    Add some pins to GPIO.
    
    Remove a high tie to the accelerometer chip select because we want to use it in 3-wire SPI mode
    
    Add SPI2 pinout file

---
+++ boards/altera/de0_nano/syn/quartus/tcl/SPI2_pin_assignments.tcl
@@ -0,0 +1,7 @@
+set_location_assignment PIN_F1 -to spi2_sd_io
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi2_sd_io
+set_location_assignment PIN_F2 -to spi2_sck_o
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi2_sck_o
+set_location_assignment PIN_G5 -to spi2_ss_o[0]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to spi2_ss_o[0]
+
